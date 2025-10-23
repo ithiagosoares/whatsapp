096c148ef686ab1280c625816012c3c85f2fc05b
Thiago s <thiago25.mss@gmail.com>
2025-10-23 23:27:32 +0000

    Se quiser matar essa pasta, dentro de (auth), além das pastas login e si

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,9 @@
 
 import { FirebaseClientProvider } from "@/firebase";
+import Icon from "@/app/icon";
+import type { ReactNode } from "react";
 
-export default function AuthLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <FirebaseClientProvider>
       <main className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
