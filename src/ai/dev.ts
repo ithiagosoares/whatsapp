a19b0cfb14594ee78fd84f39c09575231f33a48a
Thiago <96388220+ithiagosoares@users.noreply.github.com>
2025-10-31 15:53:20 -0300

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,5 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/generate-message-templates.ts';
-import '@/ai/flows/improve-template-with-ai.ts';
--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-296644579-18969",
-  "appId": "1:814805864825:web:f8488613ee3fdc818d5fa8",
-  "apiKey": "AIzaSyBSVdKZiro4GcjbHlw0XcoaK-j6BD3DEUs",
-  "authDomain": "studio-296644579-18969.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "814805864825"
-};
